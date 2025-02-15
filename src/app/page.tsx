64e58bef8394894abf71cf21949142f321434598
Ayaan <ayaan@Ayaans-MBP.bbrouter>
2025-02-16 02:54:43 +0530

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-  <div className="flex flex-col">
+  <div className="">
     <TopSection/>
     <SecondView/>
     <ThirdView/>
